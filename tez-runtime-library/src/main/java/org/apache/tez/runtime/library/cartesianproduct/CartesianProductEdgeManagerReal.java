096d433777408fa732083c79267023abcd4a2deb
Zhiyuan Yang <sjtu.yzy@gmail.com>
2017-05-11 20:15:26 -0700

    TEZ-3708. Improve parallelism and auto grouping of unpartitioned cartesian product (zhiyuany)
    Change-Id: If1c929235b473dfc0bee7a7852abd89b62ef51f6

--- tez-runtime-library/src/main/java/org/apache/tez/runtime/library/cartesianproduct/CartesianProductEdgeManagerReal.java
+++ tez-runtime-library/src/main/java/org/apache/tez/runtime/library/cartesianproduct/CartesianProductEdgeManagerReal.java
@@ -19,6 +19,7 @@ package org.apache.tez.runtime.library.cartesianproduct;
 
 import org.apache.tez.dag.api.EdgeManagerPluginContext;
 import org.apache.tez.dag.api.EdgeManagerPluginOnDemand.EventRouteMetadata;
+import org.apache.tez.runtime.library.cartesianproduct.CartesianProductUserPayload.CartesianProductConfigProto;
 
 /**
  * base class of cartesian product edge manager implementation
@@ -34,7 +35,7 @@ abstract class CartesianProductEdgeManagerReal {
     return this.context;
   }
 
-  public abstract void initialize(CartesianProductEdgeManagerConfig config) throws Exception;
+  public abstract void initialize(CartesianProductConfigProto config) throws Exception;
 
   public void prepareForRouting() throws Exception {}
 
